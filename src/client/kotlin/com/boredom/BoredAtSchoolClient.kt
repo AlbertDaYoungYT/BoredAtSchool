12739c404c84125baad666c44b614ba427951086
Albert Knudsen <kcobain465@gmail.com>
2024-09-03 08:35:42 +0000

    Init

---
+++ src/client/kotlin/com/boredom/BoredAtSchoolClient.kt
@@ -0,0 +1,9 @@
+package com.boredom
+
+import net.fabricmc.api.ClientModInitializer
+
+object BoredAtSchoolClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/com/boredom/BoredAtSchoolDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.boredom
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object BoredAtSchoolDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
